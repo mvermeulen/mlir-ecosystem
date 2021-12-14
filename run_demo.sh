a8e4f929424c92095e481a4a1ff5e01bb69cb92b
Mike Vermeulen <mike@vermeulen.com>
2021-12-14 14:16:28 -0600

    Update examples.

--- run_demo.sh
+++ run_demo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "Torchscript to MLIR demo"
-
+echo
+echo "Resnet50 using MLIR and CPU"
 cd torch-mlir
 python examples/torchscript_resnet18_e2e.py
+echo
+echo "End to End tests"
+python -m e2e_testing.torchscript.main --filter Conv2d --verbose
 
